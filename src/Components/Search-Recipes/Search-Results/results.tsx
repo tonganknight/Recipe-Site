f21791157a05be089c245644225abd3b670a7d6d
Malachi Alusa <malachialusa@gmail.com>
2024-05-25 22:09:35 -0600

    I added object mapping for nutrition info in sub menu

--- src/Components/Search-Recipes/Search-Results/results.tsx
+++ src/Components/Search-Recipes/Search-Results/results.tsx
@@ -12,9 +12,25 @@ export const SearchResults = (returnedData: any) => {
     (data: any, index: any) => {
       const foodPic = data.recipe.image;
       const cookName = data.recipe.source;
+      const caloriesAmount = data.recipe.calories;
+      const caloriesLabel = "Calories";
+      const nutrition = data.recipe.totalNutrients;
+      // const nutritonnArray = [];
+      // nutritonnArray.push(nutrition);
+      // console.log("nutritonnArray", nutritonnArray[0]);
       const onClick = () => {
         window.open(`${returnedData?.returnedData?.hits[index]?.recipe.url}`);
       };
+      const mapNutrition = Object.entries(nutrition).map(
+        ([key, nutrient], index) => {
+          return (
+            <div className="row" key={index}>
+              <div className="col">{(nutrient as any).label}</div>
+              <div className="col">{(nutrient as any).quantity}</div>
+            </div>
+          );
+        }
+      );
 
       console.log("foodPic", foodPic);
       return (
@@ -48,7 +64,6 @@ export const SearchResults = (returnedData: any) => {
                 <div className="cook-TitleFlex">
                   <strong>{`A Creation By ${cookName}`}</strong>
                 </div>
-
                 {data.recipe.ingredientLines.map((ingredient: any) => {
                   return (
                     <ul className="list-group">
@@ -56,6 +71,36 @@ export const SearchResults = (returnedData: any) => {
                     </ul>
                   );
                 })}
+                <div
+                  className="accordion accordion-flush "
+                  id="accordionExample"
+                >
+                  <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingOne">
+                      <button
+                        className="accordion-button collapseBarNutrition collapsed accordion-button.collapsed  "
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target={`#collapse2${index}`}
+                        aria-expanded="true"
+                        aria-controls={`collapse2${index}`}
+                      >
+                        Nutrition Info
+                      </button>
+                    </h2>
+                    <div
+                      id={`collapse2${index}`}
+                      className="accordion-collapse collapse"
+                      aria-labelledby="headingOne"
+                      data-bs-parent="#accordionExample"
+                    >
+                      <div className="accordion-body">
+                        <div className="container">{mapNutrition}</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
                 <div className="goLinkFlex">
                   <button onClick={onClick} className="goToLink">
                     Go to Recipe
