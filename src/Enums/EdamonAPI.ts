b2d8903b7129979a3bb3d413300cacacd98c9553
Malachi Alusa <malachialusa@gmail.com>
2025-04-03 16:31:02 -0600

    API update

--- src/Enums/EdamonAPI.ts
+++ src/Enums/EdamonAPI.ts
@@ -1,5 +1,5 @@
 export enum EdamonAPI  {
-    search = 'https://api.edamam.com/search?q=',
+    search = 'https://api.edamam.com/api/recipes/v2?type=public&beta=false&q=',
     appID = '&app_id=ac20826b',
     appKey = '&app_key=082416b66b35b0934698ef863239adfc'
 }
